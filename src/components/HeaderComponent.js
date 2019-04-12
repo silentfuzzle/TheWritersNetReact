a3f1ec39f420394ae497a1d5f9a9de00214ba1d2
Emily Palmieri <emily.palmieri@hotmail.com>
2019-04-12 12:16:23 -0700

    -Set up packages -Started implementing website navigation bar

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen: false,
+            isLoggedIn: false
+        };
+
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar dark expand="sm" color="dark" className="fixed-top">
+                    <div className="container">
+                        <NavbarBrand href="/">The Writer's Net</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/library">Library</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './HeaderComponent';
+
+function Main(props) {
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
+}
+
+export default Main;
