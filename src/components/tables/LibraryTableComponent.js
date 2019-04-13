941259e808a883425037325523cb253ac976e5db
Emily Palmieri <emily.palmieri@hotmail.com>
2019-04-13 00:03:22 -0700

    Library Table experimenting

--- src/components/tables/LibraryTableComponent.js
+++ src/components/tables/LibraryTableComponent.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
+import { Progress } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import TableContainer from './TableContainerComponent';
 
 class LibraryTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            books: [
+                {
+                    id: 0,
+                    title: 'Pajar',
+                    subtitle: 'science fantasy',
+                    author: 'Robert Gates',
+                    length: 50,
+                    ratingref: 3
+                }
+            ]
+        };
+
+        this.renderBody = this.renderBody.bind(this);
+    }
+
     renderHead() {
         return (
             <React.Fragment>
-                <th>Test1</th>
-                <th>Test2</th>
+                <th><a href="#">Title</a></th>
+                <th><a href="#">Short Description</a></th>
+                <th><a href="#">Author</a></th>
+                <th><a href="#">Length</a></th>
+                <th><a href="#">Rating</a></th>
             </React.Fragment>
         );
     }
 
     renderBody() {
+        const books = this.state.books.map((book) => {
+            return (
+                <tr key={book.id}>
+                    <th><Link to={`/book/${book.id}`}>{book.title}</Link></th>
+                    <td>{book.subtitle}</td>
+                    <td>{book.author}</td>
+                    <td><Progress value={book.length} color="dark" /></td>
+                    <td>{book.ratingref}/5</td>
+                </tr>
+            );
+        });
+
         return (
-            <tr>
-                <th>Test1</th>
-                <td>Test2</td>
-            </tr>
+            <React.Fragment>
+                {books}
+            </React.Fragment>
         );
     }
 
